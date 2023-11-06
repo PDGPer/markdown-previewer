3196d09b78846e5f81767f5f6c97117ca6d3064f
Pedro Pereira <pdgper@gmail.com>
2023-11-06 18:14:40 +0000

    Added styling using regular CSS

--- src/App.tsx
+++ src/App.tsx
@@ -24,19 +24,10 @@ function App() {
   );
 
   return (
-    <>
-      <div className="flex">
-        <div className="w-1/2">
-          <textarea id="editor" className="p-6 w-full min-h-screen resize-none focus:outline-none" value={text} onChange={(e) => setText(e.target.value)}></textarea>
-        </div>
-        <div id="preview" className="p-6 w-1/2 border-l border-gray-300 prose prose-heading:m-t-0">
-          <Markdown>{text}</Markdown>
-        </div>
-      </div>
-
-      {/* BACKGROUND */}
-      <div className="fixed top-0 w-screen h-screen -z-10"></div>
-    </>
+    <div className="container">
+      <textarea id="editor" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+      <Markdown id="preview">{text}</Markdown>
+    </div>
   );
 }
 
